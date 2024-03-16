b96a0faf68446fabb04985095fdceedb6debbd68
kevinyan2025 <yan767957598@gmail.com>
2024-03-16 18:54:53 -0400

    init commit

---
+++ queuein-backend/src/main/java/com/queuein/queueinbackend/QueueinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.queuein.queueinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueinBackendApplication.class, args);
+	}
+
+}
---
+++ queuein-backend/src/test/java/com/queuein/queueinbackend/QueueinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queuein.queueinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

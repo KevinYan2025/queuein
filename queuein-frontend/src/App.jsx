6af40d4efffc719c7d99ede74037f452e413d713
kevinyan2025 <yan767957598@gmail.com>
2024-03-17 22:35:50 -0400

    complete createQueue page

--- queuein-frontend/src/App.jsx
+++ queuein-frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import Hero from './landing/user/hero.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import CreateQueue from './landing/owner/CreateQueue.jsx'
 
 function App() {
 
   return (
-    <>
-      <div className="app">
-      <Hero />
-      {/* Other components */}
-    </div>
-  
-    </>
+    <BrowserRouter>
+      <Routes>
+            <Route path="/hero" element={<Hero/>}/>
+            <Route path="/createQueue" element={<CreateQueue/>}/>
+      </Routes>
+        </BrowserRouter>
   )
 }
 
--- queuein-frontend/src/landing/user/hero.jsx
+++ queuein-frontend/src/landing/user/hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
     <div className="hero" >
       <h1 className="title">V-Queue !</h1>
       <div className="input-wrapper">
-        <input type="text" className="inputbox" placeholder="Enter 6 digit pin" />
-        <input type="text" className="inputbox" placeholder="Enter name" />
+        <input type="text" className="inputbox" placeholder="Enter 6 digit pin" required/>
+        <input type="text" className="inputbox" placeholder="Enter name" required/>
         <button className="enterBtn">Enter</button>
         <button className="cyoBtn cyoText" > Create your own queue</button>
     </div>

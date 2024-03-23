5762e010b773710f679b77ecbca4f6791358eb82
ShanaFlash <nancy17flash@gmail.com>
2024-03-22 23:56:58 -0400

    modified user hero add new user and create new cure btn

--- queuein-frontend/src/landing/user/hero.jsx
+++ queuein-frontend/src/landing/user/hero.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import './hero.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 const Hero = () => {
+
+  const [pin, setPin] = useState('')
+  const [name, setName] = useState('')
+  const navigate = useNavigate()
+
+  const handleJoin = () => {
+    console.log('Joining queue')
+    axios.post('http://localhost:8080/createAndAddUserToQueue', null, {
+      params: {
+        queueID: pin,
+        name: name
+      }
+    })
+    .then(response => {
+      console.log('Joined queue successfully:', response.data.id);
+      navigate(`/userqueue/${response.data.id}`)
+    })
+    .catch(error => {
+      console.error('Error joining queue:', error);
+    });
+  }
+
+  const handleCreate = () => {  
+    console.log('Creating queue')
+    navigate('/createqueue')
+  }
+
+
   return (
     <div className="hero" >
       <h1 className="title">V-Queue !</h1>
       <div className="input-wrapper">
-        <input type="text" className="inputbox" placeholder="Enter 6 digit pin" required/>
-        <input type="text" className="inputbox" placeholder="Enter name" required/>
-        <button className="enterBtn">Enter</button>
-        <button className="cyoBtn cyoText" > Create your own queue</button>
+        <input type="text" className="inputbox" placeholder="Enter 6 digit pin" onChange={event=>(setPin(event.target.value))} required/>
+        <input type="text" className="inputbox" placeholder="Enter name" onChange={event=>(setName(event.target.value))} required/>
+        <button className="enterBtn" onClick={handleJoin}>Enter</button>
+        <button className="cyoBtn cyoText" onClick={handleCreate}> Create your own queue</button>
     </div>
     </div>
    
